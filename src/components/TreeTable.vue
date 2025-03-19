5a8a14fc1be3eb77d2401599a6eac959137c2e52
nice-witch <78809819+nice-witch@users.noreply.github.com>
2025-03-19 19:19:01 +0500

    fix: write data for tree structure

--- src/components/TreeTable.vue
+++ src/components/TreeTable.vue
@@ -1,30 +1,14 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue'
 import { useTreeStore } from "@/stores/useTreeStore";
-import { AllCommunityModule, ModuleRegistry, RowGroupingDisplayType } from 'ag-grid-community';
-import {  RowGroupingModule,} from 'ag-grid-enterprise';
+import { AllCommunityModule, ModuleRegistry, GetDataPath } from 'ag-grid-community';
+import {  RowGroupingModule, TreeDataModule} from 'ag-grid-enterprise';
 import { AgGridVue } from "ag-grid-vue3";
 
-ModuleRegistry.registerModules([AllCommunityModule, RowGroupingModule]);
+ModuleRegistry.registerModules([AllCommunityModule, RowGroupingModule, TreeDataModule]);
 
 const treeStory = useTreeStore();
-const groupDefaultExpanded = ref(-1);
-const columnDefs = ref([
-  { field: "id", headerName: "№", lockPosition: 'left', width: 60},
-  { field: "parent", rowGroup: true, hide: true },
-  { field: "label", headerName: "Наименование", width: 440  /*aggFunc: 'first'*/},
-]);
-
-// const defaultColDef = {
-//   flex: 1,
-// };
-
-const autoGroupColumnDef = {
-  headerName: 'Категория',
-  width: 400,
-  field: "parent",
-  cellRenderer: "agGroupCellRenderer",
-}
+const rowData = computed(() => processData(treeStory.treeStore.getAll()));
 
 const rowSelection = {
   mode: "multiRow",
@@ -33,15 +17,70 @@ const rowSelection = {
   headerCheckbox: false,
 }
 
-const rowData = computed(() => treeStory.treeStore.getAll());
+const groupDefaultExpanded = ref(-1);
+const columnDefs = ref([
+  {
+    field: "id",
+    headerName: "№",
+    lockPosition: 'left',
+    width: 60,
+
+  },
+  {
+    field: "label",
+    headerName: "Наименование",
+    width: 440,
+    filterParams: {
+      treeList: true,
+
+    }
+  },
+]);
+
+const autoGroupColumnDef = {
+  headerName: 'Категория',
+  width: 400,
+  field: "category",
+  cellRendererParams: {
+    suppressCount: true,
+  },
+  filterParams: {
+    treeList: true
+  }
+}
+
+const getDataPath = ref<GetDataPath>((data) => {
+  return data.dataPath;
+});
+
+function processData(data) {
+  const flattenedData = [];
+
+  const row = (row) => {
+    const category = treeStory.treeStore.getChildren(row.id).length ? 'Группа' : 'Элемент';
+    const dataPath = [];
+
+    if(treeStory.treeStore.getAllParents(row.id).length) {
+      treeStory.treeStore.getAllParents(row.id).map(el =>dataPath.push(el.id))
+    } else {
+      dataPath.push(row.id)
+    }
+
+    flattenedData.push({...row, dataPath: dataPath.reverse(), category})
+  }
+
+  data.forEach((item) => row(item))
+  return flattenedData;
+}
+
 </script>
 
 <template>
   <AgGridVue
     style="width: 100%; height: 90%"
     :columnDefs="columnDefs"
-    :defaultColDef="defaultColDef"
-    :isGroupOpenByDefault="isGroupOpenByDefault"
+    :treeData="true"
+    :getDataPath="getDataPath"
     :autoGroupColumnDef="autoGroupColumnDef"
     :rowSelection="rowSelection"
     :suppressAggFuncInHeader="true"
